ab69e92dbc89793c15d9a0a62e0039ac6fe8eb18
Tarık GÜLYUVA <tarikgulyuva@hotmail.com>
2021-06-13 02:39:00 +0300

    Add Utilities/Results and Constants/Messages

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LayeredArchitectureExample.Core.Utilities.Results;
 using LayeredArchitectureExample.Entities.DTOs;
 
 namespace LayeredArchitectureExample.Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
-        List<Product> GetAllByCategoryId(int id);
-        List<Product> GetByUnitPrice(decimal min,decimal max);
-        List<ProductDetailDto> GetProductDetails();
+        IDataResult<List<Product>> GetAll();
+        IDataResult<List<Product>> GetAllByCategoryId(int CategoryId);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min,decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IResult Add(Product product);
+        IDataResult<Product> GetById(int productId);
     }
 }
