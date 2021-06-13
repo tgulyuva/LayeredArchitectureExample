ffb996594d558b83b582d88eb115ab9ab3f40549
Tarık GÜLYUVA <tarikgulyuva@hotmail.com>
2021-06-13 19:33:13 +0300

    add webapi project

---
+++ LayeredArchitectureExample.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LayeredArchitectureExample.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public string Get() => "merhaba";
+    }
+}
