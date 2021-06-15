c55f34740625710d42219c92a42e87960d72d959
Tarık GÜLYUVA <tarikgulyuva@hotmail.com>
2021-06-15 19:29:50 +0300

    Add Autofac

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using LayeredArchitectureExample.Business.Abstract;
+using LayeredArchitectureExample.Business.Concrete;
+using LayeredArchitectureExample.DataAccess.Abstract;
+using LayeredArchitectureExample.DataAccess.Concrete.EntityFramework;
+
+namespace LayeredArchitectureExample.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
