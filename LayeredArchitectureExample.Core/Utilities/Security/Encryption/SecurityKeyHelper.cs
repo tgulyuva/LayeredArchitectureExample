3091c4626413fc5ea4288d260e826adc77a13165
Tarık GÜLYUVA <tarikgulyuva@hotmail.com>
2021-06-22 00:41:39 +0300

    added security classes

---
+++ LayeredArchitectureExample.Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LayeredArchitectureExample.Core.Utilities.Security.Encryption
+{
+    public class SecurityKeyHelper
+    {
+        public static SecurityKey CreateSecurityKey(string securityKey) => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+    }
+}
---
+++ LayeredArchitectureExample.Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LayeredArchitectureExample.Core.Utilities.Security.Encryption
+{
+  public  class SigningCredentialsHelper
+    {
+        public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey) =>
+            new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+    }
+}
