8b82768165cac317c2e17fc354eb8ea235a81612
Tarık GÜLYUVA <tarikgulyuva@hotmail.com>
2021-06-23 00:57:39 +0300

    added Auth controller and modified startup for Authentication

--- LayeredArchitectureExample.WebAPI/Startup.cs
+++ LayeredArchitectureExample.WebAPI/Startup.cs
@@ -12,8 +12,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using LayeredArchitectureExample.Business.Abstract;
 using LayeredArchitectureExample.Business.Concrete;
+using LayeredArchitectureExample.Core.Utilities.Security.Encryption;
+using LayeredArchitectureExample.Core.Utilities.Security.JWT;
 using LayeredArchitectureExample.DataAccess.Abstract;
 using LayeredArchitectureExample.DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LayeredArchitectureExample.WebAPI
 {
@@ -32,6 +36,22 @@ namespace LayeredArchitectureExample.WebAPI
             services.AddControllers();
             //services.AddSingleton<IProductService, ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
+            var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = tokenOptions.Issuer,
+                        ValidAudience = tokenOptions.Audience,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +65,8 @@ namespace LayeredArchitectureExample.WebAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
