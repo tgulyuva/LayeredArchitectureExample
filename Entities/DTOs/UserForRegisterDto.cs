8a7e86cb8c516ee4dbd5e8dd8900597347dd0571
Tarık GÜLYUVA <tarikgulyuva@hotmail.com>
2021-06-23 00:54:50 +0300

    added DTOs User login and register

---
+++ Entities/DTOs/UserForRegisterDto.cs
@@ -0,0 +1,12 @@
+﻿using LayeredArchitectureExample.Core.Entities;
+
+namespace LayeredArchitectureExample.Entities.DTOs
+{
+    public class UserForRegisterDto : IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
